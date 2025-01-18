a473283f5ebc759cfcb70dd361bbb7bbbb444342
Brenda Franco <brenda_olifranco@hotmail.com>
2025-01-18 11:56:08 -0300

    resultado final esperado

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -48,8 +48,7 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         cy.get('#terms').click()
         cy.get('#place_order').click()
 
-
-
+        cy.get('.page-title').should('contain', 'Pedido recebido')
 
             
   });
